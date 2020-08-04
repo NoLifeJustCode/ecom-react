1329890a7737344fc99cbe5e2840557b717e1228
User <saravananr728@gmail.com>
2020-08-04 19:29:03 +0530

    Updates

---
+++ precache-manifest.6d64916b80e14367bf741421c21de8c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c40c066c304595e7790c0533d3eec2",
+    "url": "/ecom-react/index.html"
+  },
+  {
+    "revision": "f6a82fc7cd1f466848e9",
+    "url": "/ecom-react/static/css/2.bbe4ddbe.chunk.css"
+  },
+  {
+    "revision": "aa96222226ea598be220",
+    "url": "/ecom-react/static/css/main.67eb0d48.chunk.css"
+  },
+  {
+    "revision": "f6a82fc7cd1f466848e9",
+    "url": "/ecom-react/static/js/2.ae018bae.chunk.js"
+  },
+  {
+    "revision": "d4bb3d8094fe18c625c65ab2b9fef078",
+    "url": "/ecom-react/static/js/2.ae018bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa96222226ea598be220",
+    "url": "/ecom-react/static/js/main.775f868c.chunk.js"
+  },
+  {
+    "revision": "9fcf61f340900ef81f22",
+    "url": "/ecom-react/static/js/runtime-main.297b685e.js"
+  },
+  {
+    "revision": "e659e200c3500b86cf1e1fd4493fa8db",
+    "url": "/ecom-react/static/media/transparent.e659e200.png"
+  }
+]);
